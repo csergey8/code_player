06044d75b5bf80548342e40577f2edcbf06e5e97
csergey8 <c.sergey@protonmail.com>
2017-09-14 17:49:32 +0300

    add finish code

--- js/app.js
+++ js/app.js
@@ -1,20 +1,40 @@
 (function ($) {
 
-  $('.btn').hover(function(){
+  function update() {
+    $('iframe').contents().find('html').html("<html><head><style type='text/css'>" + $("#cssPanel").val() + "</style></head><body>" + $('#htmlPanel').val() + "</body></html>");
+
+    document.getElementById("outputPanel").contentWindow.eval($('#javascriptPanel').val());
+
+
+  };
+
+  $('.btn').hover(function () {
 
     $(this).addClass("btn-highlight");
-  }, function() {
+  }, function () {
     $(this).removeClass("btn-highlight");
   });
 
-  $('.btn').click(function(){
+  $('.btn').click(function () {
     $(this).toggleClass("active");
+
+    var panelId = $(this).attr('id') + 'Panel';
+
+    $('#' + panelId).toggleClass('hidden');
+
+    var panels = 4 - $('.hidden').length;
+
+    $('textarea').width(($(window).width() / panels) - 100);
+
+    update();
+
   });
 
 
 
-
-
+  $('textarea').on('change keyup paste', function () {
+    update();
+  });
 
 
 
